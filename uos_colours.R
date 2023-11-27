431171ad8c3182ca11a835cb1963b83610d5f9c9
markdunning <mark.dunning@gmail.com>
2023-11-27 10:55:24 +0000

    add UoS colour definitions

---
+++ uos_colours.R
@@ -0,0 +1,17 @@
+
+uos_colours <- c("#440099","#9ADBE8","#131E29",
+                 "#005A8F",
+                 "#00BBCC",
+                 "#64CBE8",
+                 "#00CE7C",
+                 "#3BD4AE",
+                 "#A1DED2",
+                 "#663DB3",
+                 "#981F92",
+                 "#DAA8E2",
+                 "#E7004C",
+                 "#FF6371",
+                 "#FF9664"
+)
+
+uos_colours <- as.character(uos_colours)
